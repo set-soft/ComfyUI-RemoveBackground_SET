5bc910f5c3f83192bcb6641b68f3fa20c576f64c
Salvador E. Tropea <salvador@inti.gob.ar>
2025-09-02 10:06:46 -0300

    Removed unused decoder blocks

--- src/nodes/birefnet_old/models/modules/decoder_blocks.py
+++ src/nodes/birefnet_old/models/modules/decoder_blocks.py
@@ -1,10 +1,5 @@
 import torch.nn as nn
 from .aspp import ASPPDeformable
-from .attentions import PSA, SGE
-from ...config import Config
-
-
-config = Config()
 
 
 class BasicDecBlk(nn.Module):
@@ -25,61 +20,4 @@ class BasicDecBlk(nn.Module):
         x = self.dec_att(x)
         x = self.conv_out(x)
         x = self.bn_out(x)
-        return x
-
-
-class ResBlk(nn.Module):
-    def __init__(self, in_channels=64, out_channels=None, inter_channels=64):
-        super(ResBlk, self).__init__()
-        if out_channels is None:
-            out_channels = in_channels
-        inter_channels = 64
-
-        self.conv_in = nn.Conv2d(in_channels, inter_channels, 3, 1, padding=1)
-        self.bn_in = nn.BatchNorm2d(inter_channels)
-        self.relu_in = nn.ReLU(inplace=True)
-        self.dec_att = ASPPDeformable(in_channels=inter_channels)
-        self.conv_out = nn.Conv2d(inter_channels, out_channels, 3, 1, padding=1)
-        self.bn_out = nn.BatchNorm2d(out_channels)
-
-        self.conv_resi = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
-
-    def forward(self, x):
-        _x = self.conv_resi(x)
-        x = self.conv_in(x)
-        x = self.bn_in(x)
-        x = self.relu_in(x)
-        x = self.dec_att(x)
-        x = self.conv_out(x)
-        x = self.bn_out(x)
-        return x + _x
-
-
-class HierarAttDecBlk(nn.Module):
-    def __init__(self, in_channels=64, out_channels=None, inter_channels=64):
-        super(HierarAttDecBlk, self).__init__()
-        if out_channels is None:
-            out_channels = in_channels
-        inter_channels = 64
-        self.split_y = 8     # must be divided by channels of all intermediate features
-        self.split_x = 8
-
-        self.conv_in = nn.Conv2d(in_channels, inter_channels, 3, 1, 1)
-
-        self.psa = PSA(inter_channels*self.split_y*self.split_x, S=config.batch_size)
-        self.sge = SGE(groups=config.batch_size)
-        self.dec_att = ASPPDeformable(in_channels=inter_channels)
-        self.conv_out = nn.Conv2d(inter_channels, out_channels, 3, 1, 1)
-
-    def forward(self, x):
-        x = self.conv_in(x)
-        N, C, H, W = x.shape
-        x_patchs = x.reshape(N, -1, H//self.split_y, W//self.split_x)
-
-        # Hierarchical attention: group attention X patch spatial attention
-        x_patchs = self.psa(x_patchs)   # Group Channel Attention -- each group is a single image
-        x_patchs = self.sge(x_patchs)   # Patch Spatial Attention
-        x = x.reshape(N, C, H, W)
-        x = self.dec_att(x)
-        x = self.conv_out(x)
         return x
