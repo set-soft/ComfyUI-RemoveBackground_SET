f747d3abafb52e9d040da5016be341bc49ba9bd9
Salvador E. Tropea <salvador@inti.gob.ar>
2025-09-09 07:54:09 -0300

    [Added] Some author information

---
+++ src/nodes/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Salvador E. Tropea
+# Copyright (c) 2025 Instituto Nacional de Tecnología Industrial
+# License: GPL-3.0
+# Project: ComfyUI-BiRefNet-SET
+from seconohe.logger import initialize_logger
+
+
+__version__ = "1.0.0"
+__copyright__ = "Copyright © 2025 Salvador E. Tropea / Instituto Nacional de Tecnología Industrial"
+__license__ = "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>"
+__author__ = "Salvador E. Tropea"
+NODES_NAME = "BiRefNet-SET"
+main_logger = initialize_logger(NODES_NAME)
