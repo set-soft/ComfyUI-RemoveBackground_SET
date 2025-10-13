b7906f74a13384502c270bc3dee7c2543bb9346a
Salvador E. Tropea <salvador@inti.gob.ar>
2025-10-13 09:36:55 -0300

    [MVANet 2 Safetensors] Adjusted to changes in RemBgArch

--- tools/mvanet2safetensors.py
+++ tools/mvanet2safetensors.py
@@ -47,16 +47,16 @@ if __name__ == "__main__":
     main_logger.info(f"Back bone type: {bb_a.bb}")
     main_logger.info(f"Model version: {bb_a.version}")
 
-    model = bb_a.instantiate_model()
-    model.load_state_dict(state_dict)
-    model.eval()
     if args.half:
-        model.to(dtype=torch.float16)
+        dtype = torch.float16
         fp = 'F16'
         dt = 'float16'
     else:
+        dtype = torch.float32
         fp = 'F32'
         dt = 'float32'
+
+    model = bb_a.instantiate_model(state_dict, dtype=dtype)
 
     metadata = {
         "desc": "MVANet base background remover",
